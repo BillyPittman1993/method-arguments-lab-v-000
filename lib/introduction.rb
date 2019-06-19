a746294bd400f3f14a73c19a94b93994a69aa8af
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-19 02:31:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,5 +6,10 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+puts "Hi, what is your name?"
 name = gets.strip
 introduction(name)
+puts "Sry, I could barely hear you, what is your name and what language do you speak?"
+name = gets.strip
+language = gets.strip
+introduction_with_language(name, language)
